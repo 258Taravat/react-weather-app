21930d520c0f4b6c4e2bab32af4a617511704ec3
Taravat Yazdani <ytaravat@yahoo.com>
2023-10-08 08:37:07 +0330

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,27 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        {" "}
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/taravat-yazdani-539a21177/"
+          target="_blank"
+        >
+          Taravat Yazdani
+        </a>{" "}
+        and it is{" "}
+        <a
+          href="https://github.com/258Taravat/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
