b7346b5cd2c090eeecb59fbff23d5fc05acadc24
Taravat Yazdani <ytaravat@yahoo.com>
2023-10-21 09:58:54 +0330

    Weather forecast was adjusted with coordinates and updated.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -13,7 +13,7 @@ export default function WeatherInfo(props) {
         </li>
         <li className="text-capitalize">{[props.data.description]}</li>
       </ul>
-      <div className="row mt-3">
+      <div className="row mt-4">
         <div className="col-6">
           <div className="clear-fix d-flex align-items-center">
             <div className="float-left">
@@ -24,9 +24,8 @@ export default function WeatherInfo(props) {
             </div>
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-6 mt-4">
           <ul>
-            <li>Precipitation: 0% </li>
             <li>Humidity: {[props.data.humidity]}%</li>
             <li>Wind: {[props.data.wind]} km/h</li>
           </ul>
